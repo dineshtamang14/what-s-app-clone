041ce4a8dbe0735c0dacc43f8d799e67be69b470
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-25 08:10:41 +0530

    added login loading component

--- components/Loading.js
+++ components/Loading.js
@@ -1,16 +1,18 @@
+import Image from "next/image";
+
 function Loading() {
     return (
-        <center>
-            <div>
-                <img
-                    src=""
-                    alt=""
-                    style={{ marginBottom: 10 }}
-                    height={200}
-                />
-            </div>
-        </center>
-    )
+      <center>
+        <div>
+          <Image
+            src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
+            alt="whatsapp-img"
+            style={{ marginBottom: 10 }}
+            height={200}
+          />
+        </div>
+      </center>
+    );
 }
 
 export default Loading;
