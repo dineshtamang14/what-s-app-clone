53b838e1f737baf2c06f719eb7603fa7c9218ff2
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-12-05 16:09:31 +0530

    update chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -3,15 +3,22 @@ import { Avatar } from "@material-ui/core";
 import { useAuthState } from "react-firebase-hooks/auth";
 import getRecipientEmail from "../utils/getRecipientEmail";
 import { auth } from "../firebase";
+import { useCollection } from "react-firebase-hooks/firestore"
 
 function Chat({ id, users }) {
     const [user] = useAuthState(auth);
 
+    const [recipientSnapshot] = useCollection(db.collection('user').where('email', '==', getRecipientEmail(users, user)));
+    const recipient = recipientSnapshot?.docs?.[0]?.data();
     const recipientEmail = getRecipientEmail(users, user);
 
     return (
         <Container>
-            <UserAvatar />
+            {recipient ? (
+                <UserAvatar src={recipient?.photoURL} />
+            ) : (
+                <UserAvatar>{recipientEmail[0]}</UserAvatar>
+            )}
             <p>{recipientEmail}</p>
         </Container>
     )
